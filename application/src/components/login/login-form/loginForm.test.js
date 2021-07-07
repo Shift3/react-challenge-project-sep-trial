f5c981a5f3bb3c3b02d9ba81e0d3f45007f594c3
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-07 15:10:57 -0700

    chore(login form test): test renders login form

--- application/src/components/login/login-form/loginForm.test.js
+++ application/src/components/login/login-form/loginForm.test.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../../../redux/store';
 
 import LoginForm from './loginForm';
 
-const onLogin = (formValue) => {
-    return { ok: 1 }
-}
+const onLogin = () => ({ ok: 1 })
+
 
 describe('Login Form', () => {
     test('form renders', () => {
         render(
-            <div />
+            <Provider store={store}>
+                <LoginForm onLogin={() => onLogin()} />
+            </Provider>
         );
+        
+        screen.debug();
     })
 });
