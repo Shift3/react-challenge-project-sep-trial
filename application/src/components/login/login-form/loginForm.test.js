5f398990ba34bba558ce068c463f10557c63fda8
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-07 16:25:37 -0700

    chore(login form test): make sure inputs and button render and that button calls the correct propr

--- application/src/components/login/login-form/loginForm.test.js
+++ application/src/components/login/login-form/loginForm.test.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../../../redux/store';
 
 import LoginForm from './loginForm';
 
-const onLogin = () => ({ ok: 1 })
+// const onLogin = () => {
+//     console.log('i fired!');
+// }
 
 
 describe('Login Form', () => {
     test('form renders', () => {
         render(
             <Provider store={store}>
-                <LoginForm onLogin={() => onLogin()} />
+                <LoginForm />
             </Provider>
         );
+
+        // there should be a properly labelled email field
+        expect(screen.getByLabelText('Email')).toBeInTheDocument();
+
+        // there should be a password field
+        expect(screen.getByLabelText('Password')).toBeInTheDocument();
+
+        // there should be a login button
+        expect(screen.getByRole('button')).toBeInTheDocument();
+    })
+
+    test('login prop fires', () => {
+        const onLogin = jest.fn();
+
+        render(
+            <Provider store={store}>
+                <LoginForm onLogin={onLogin} />
+            </Provider>
+        );
+
+        fireEvent.click(screen.getByRole('button'));
+        expect(onLogin).toHaveBeenCalled();
         
-        screen.debug();
     })
 });
