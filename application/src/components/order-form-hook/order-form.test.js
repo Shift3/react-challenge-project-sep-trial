f49d91900e05c66c244411b26b4b9cddcdffe798
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-08 14:28:18 -0700

    chore(order form test): form renders

--- application/src/components/order-form-hook/order-form.test.js
+++ application/src/components/order-form-hook/order-form.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
 
 import OrderForm from './order-form';
 
 describe('Order Form', () => {
     test('renders order form', () => {
-        render(<div />);
+        render(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <OrderForm />
+                </MemoryRouter>
+            </Provider>
+        );
 
-
+        screen.debug();
     })
 });
