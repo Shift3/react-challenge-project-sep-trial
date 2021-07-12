53b7abfead5fb5a1025d46fa1d9f7e5a34599bc3
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-12 10:49:03 -0700

    chore(view orders test): successfully tests order rendering

--- application/src/components/view-orders-hook/ordersList.test.js
+++ application/src/components/view-orders-hook/ordersList.test.js
@@ -9,9 +9,50 @@ describe('Orders List', () => {
             <OrdersList
             />
         )
-        screen.debug();
-        const emptyDiv = document.getElementsByClassName('empy-orders');
+        const emptyDiv = document.getElementsByClassName('empty-orders');
         const arr = Array.from(emptyDiv);
         expect(arr.length).toBe(1);
+    });
+
+    test('renders one order', () => {
+        const orders = [
+            {
+                order_item: "Food",
+                quantity: "777",
+                _id: 1
+            }
+        ];
+        render(
+            <OrdersList
+                orders={orders}
+            />
+        )
+        expect(screen.getByText('Food')).toBeInTheDocument();
+        expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
+    });
+
+    test('renders multiple orders', () => {
+        const orders = [
+            {
+                order_item: "Food",
+                quantity: "777",
+                _id: 1
+            },
+            {
+                order_item: "Drink",
+                quantity: "888",
+                _id: 2
+            }
+        ];
+        render(
+            <OrdersList
+                orders={orders}
+            />
+        )
+        expect(screen.getByText('Food')).toBeInTheDocument();
+        expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
+        expect(screen.getByText('Drink')).toBeInTheDocument();
+        expect(screen.getByText(/^.*888.*$/gm)).toBeInTheDocument();
+
     });
 })
