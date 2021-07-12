fa239ed7104d2c06e70e9d2bb6b8a3cc32323bad
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-12 10:20:42 -0700

    refac(view orders): refactor order list to its own component for testing

--- application/src/components/view-orders-hook/viewOrders.js
+++ application/src/components/view-orders-hook/viewOrders.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Template } from '../../components';
 import { SERVER_IP } from '../../private';
+import OrdersList from './ordersList';
 import './viewOrders.css';
 
 export default function ViewOrders(props) {
@@ -21,25 +22,9 @@ export default function ViewOrders(props) {
     return (
         <Template>
             <div className="container-fluid">
-                {orders.map(order => {
-                    const createdDate = new Date(order.createdAt);
-                    return (
-                        <div className="row view-order-container" key={order._id}>
-                            <div className="col-md-4 view-order-left-col p-3">
-                                <h2>{order.order_item}</h2>
-                                <p>Ordered by: {order.ordered_by || ''}</p>
-                            </div>
-                            <div className="col-md-4 d-flex view-order-middle-col">
-                                <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
-                                <p>Quantity: {order.quantity}</p>
-                            </div>
-                            <div className="col-md-4 view-order-right-col">
-                                <button className="btn btn-success">Edit</button>
-                                <button className="btn btn-danger">Delete</button>
-                            </div>
-                        </div>
-                    );
-                })}
+                <OrdersList
+                    orders={orders}
+                />
             </div>
         </Template>
     );
