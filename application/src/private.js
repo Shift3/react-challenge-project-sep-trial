dda6fbb740e1229cf569b082d2e27ed834967dd7
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-07-13 15:42:19 -0700

    chore(private): update server IP to user localhost regardless, comment out line for docker toolboz

--- application/src/private.js
+++ application/src/private.js
@@ -1,3 +1,6 @@
 const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
 
-export const SERVER_IP = windowsPlatforms.indexOf(window.navigator.platform) < 0 ? 'http://localhost:4000' : 'http://192.168.99.100:4000';
+export const SERVER_IP = "http://localhost:4000";
+
+// Use this line instead of the above if using Docker Toolbox
+// export const SERVER_IP = windowsPlatforms.indexOf(window.navigator.platform) < 0 ? 'http://localhost:4000' : 'http://192.168.99.100:4000';
