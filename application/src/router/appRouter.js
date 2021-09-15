098e2ba1b35c2ba0e00a34d0c0c0e807669faae3
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-09-14 17:11:15 -0700

    fix(order-form): add the target method to target the value of the event

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, Login, OrderFormHook, ViewOrdersHook } from '../components';
+import { Main, Login, OrderForm, ViewOrdersHook } from '../components';
 
 const AppRouter = (props) => {
   return (
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderFormHook} />
+      <Route path="/order" exact component={OrderForm} />
       <Route path="/view-orders" exact component={ViewOrdersHook} />
     </Router>
   );
