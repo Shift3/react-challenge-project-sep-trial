fc1dad29f7bb509d970a615dadad2cf4541681f7
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-09-16 13:36:16 -0700

    feat(template): add user's email at top of page when logged in

--- application/src/components/common/template.js
+++ application/src/components/common/template.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Nav } from '../../components';
-import './template.css';
+import React from "react";
+import { Nav } from "../../components";
+import { useSelector } from "react-redux";
+import "./template.css";
 
-const Template = props => {
-    return (
-        <div className="bg-layer">
-            <div className="fg-layer">
-                <label className="logo">Bruce's Diner</label>
-                <Nav />
-                {props.children}
-            </div>
-        </div>
-    );
-}
+const Template = (props) => {
+  const email = useSelector((state) => state.login.email);
+
+  return (
+    <div className="bg-layer">
+      <div className="fg-layer">
+        {email.length > 0 ? <div className="user-email">User: {email}</div> : null}
+        <label className="logo">Bruce's Diner</label>
+        <Nav />
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
 export default Template;
