c28a6ab8ee5b86995107a68976f7db5064214ce4
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-09-16 13:38:46 -0700

    style(template): format code using prettier

--- application/src/components/common/template.js
+++ application/src/components/common/template.js
@@ -9,7 +9,11 @@ const Template = (props) => {
   return (
     <div className="bg-layer">
       <div className="fg-layer">
-        {email.length > 0 ? <div className="user-email">User: {email}</div> : <div className="user-email">User: Not Logged In</div>}
+        {email.length > 0 ? (
+          <div className="user-email">User: {email}</div>
+        ) : (
+          <div className="user-email">User: Not Logged In</div>
+        )}
         <label className="logo">Bruce's Diner</label>
         <Nav />
         {props.children}
