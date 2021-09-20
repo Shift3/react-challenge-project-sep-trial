8b01e045425923f87c2702e135c3771b5170d51a
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-09-20 16:16:23 -0700

    refactor(nav): remove the old route guard

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { logout } from "../login/loginSlice";
 
 const Nav = () => {
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector(state => state.login.isLoggedIn);
 
   return (
     <div className="nav-strip">
-      <Link to={isLoggedIn ? "/order" : "/login"} className="nav-link">
+      <Link to={"/order" } className="nav-link">
         <div className="nav-link-style">
           <label className="nav-label">Order Form</label>
         </div>
       </Link>
-      <Link to={isLoggedIn ? "/view-orders" : "/login"} className="nav-link" id="middle-link">
+      <Link to={"/view-orders"} className="nav-link" id="middle-link">
         <div className="nav-link-style">
           <label className="nav-label">View Orders</label>
         </div>
