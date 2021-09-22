d4216b2653b157be1e50c49269a03b506518e6d4
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-09-22 15:29:52 -0700

    Update App.js

--- application/src/App.js
+++ application/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import AppRouter from './router/AppRouter';
 import { store } from './redux/store';
-import './App.css';
 
 class App extends Component {
   render() {
