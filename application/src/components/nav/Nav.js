bd6b88ad45b97bffc3e7e87a28531dcfdd8f1f52
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-09-22 16:04:08 -0700

    feat(redux-persist): install and implement redux persist

--- application/src/components/nav/Nav.js
+++ application/src/components/nav/Nav.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import "./nav.css";
 import { useDispatch } from "react-redux";
 import { logout } from "../login/loginSlice";
+import { persistor } from "../../redux/store";
 
 const Nav = () => {
   const dispatch = useDispatch();
 
   return (
     <div className="nav-strip">
-      <Link to={"/order" } className="nav-link">
+      <Link to={"/order"} className="nav-link">
         <div className="nav-link-style">
           <label className="nav-label">Order Form</label>
         </div>
@@ -20,7 +21,13 @@ const Nav = () => {
         </div>
       </Link>
       <Link to={"/"} className="nav-link">
-        <div className="nav-link-style" onClick={() => dispatch(logout())}>
+        <div
+          className="nav-link-style"
+          onClick={() => {
+            persistor.purge();
+            dispatch(logout());
+          }}
+        >
           <label className="nav-label">Log Out</label>
         </div>
       </Link>
