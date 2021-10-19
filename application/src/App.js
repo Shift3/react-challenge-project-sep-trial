5e989163bcc65031d34bb9ae2d9a7750453dae3b
angelz510 <angelzrod4@gmail.com>
2021-10-19 13:36:25 -0700

    chore: setting up dev branch

--- application/src/App.js
+++ application/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import AppRouter from './router/appRouter';
-import store from './redux/store';
-import './App.css';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import AppRouter from "./router/appRouter";
+import store from "./redux/store";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -10,7 +10,7 @@ class App extends Component {
       <Provider store={store}>
         <AppRouter />
       </Provider>
-    )
+    );
   }
 }
 
