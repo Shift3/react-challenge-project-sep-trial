823410e580e83a9ca7a02ec64eff3ca5e26b54c2
angelz510 <angelzrod4@gmail.com>
2021-10-20 15:33:10 -0700

    fix(logout): fixed logout button to redirect and clear login info on redux
    used dispatch to logout onclick and clear user info on redux

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../redux/actions/authActions";
 
 const Nav = (props) => {
-    return (
-        <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
-                </div>
-            </Link>
-            <Link to={"/view-orders"} className="nav-link" id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <Link to={"/login"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </Link>
+  const dispatch = useDispatch();
+
+  return (
+    <div className="nav-strip">
+      <Link to={"/order"} className="nav-link">
+        <div className="nav-link-style">
+          <label className="nav-label">Order Form</label>
         </div>
-    );
-}
+      </Link>
+      <Link to={"/view-orders"} className="nav-link" id="middle-link">
+        <div className="nav-link-style">
+          <label className="nav-label">View Orders</label>
+        </div>
+      </Link>
+      <Link to={"/"} className="nav-link">
+        <div className="nav-link-style">
+          <label className="nav-label" onClick={() => dispatch(logoutUser())}>
+            Log Out
+          </label>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
 export default Nav;
