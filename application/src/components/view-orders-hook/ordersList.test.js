23c78969dc5c2421b210ef8ebbef06d35ab590f7
andrewjspivey <12spivey@gmail.com>
2021-10-20 12:52:31 -0700

    test(ordersList): adds test for checking that order times are correctly formatted to hh:mm:ss. task #3

--- application/src/components/view-orders-hook/ordersList.test.js
+++ application/src/components/view-orders-hook/ordersList.test.js
@@ -55,4 +55,30 @@ describe('Orders List', () => {
         expect(screen.getByText(/^.*888.*$/gm)).toBeInTheDocument();
 
     });
+
+    test('renders orders with hh:mm:ss time format', () => {
+        const orders = [
+            {
+                order_item: "Food",
+                quantity: "777",
+                _id: 1,
+                createdAt: "2021-10-19T23:01:08.000Z" // this is iso 8601 format of 16:01:08
+            },
+            {
+                order_item: "Food",
+                quantity: "888",
+                _id: 2,
+                createdAt: "2021-10-19T21:03:04.000Z"
+            }
+        ];
+        render(
+            <OrdersList
+                orders={orders}
+            />
+        )
+        expect(screen.getByText(/^.*16:01:08.*$/gm)).toBeInTheDocument(); // formatted time of orders.createdAt
+        expect(screen.getByText(/^.*14:03:04.*$/gm)).toBeInTheDocument(); // formatted time of orders.createdAt
+        expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
+        expect(screen.getByText(/^.*888.*$/gm)).toBeInTheDocument();
+    });
 })
