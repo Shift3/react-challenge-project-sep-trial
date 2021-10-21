d2d7f65533585ce8507a7ab9d1ec322820c10b36
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2021-10-20 17:35:27 -0700

    fix(authReducer): Fixed syntax error

--- application/src/redux/reducers/authReducer.js
+++ application/src/redux/reducers/authReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = { email: null, token: null };
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case LOGIN:
-            return { ...state, email: action.payload.login, token: action.payload.token }
+            return { ...state, email: action.payload.email, token: action.payload.token }
         case LOGOUT:
             return { ...state, ...INITIAL_STATE }
         default:
--- application/src/redux/store.js
+++ application/src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
-
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(ReduxThunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 export default store;
